2f1e15c60547b07d460ed881d66462d30e10aacc
nxdavar <nealrr01@gmail.com>
2022-06-14 02:02:59 -0400

    Time: 75 ms (41.52%), Space: 15.3 MB (78.34%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -0,0 +1,24 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        initial = {}
+        freqMap = {}
+        result = ""
+        
+        for i in range(len(s)):
+            if s[i] not in initial: 
+                initial[s[i]] = 0
+            initial[s[i]] += 1
+        
+        for key in initial.keys():
+            if initial[key] not in freqMap: 
+                freqMap[initial[key]] = []
+            curr = freqMap[initial[key]]
+            curr.append(key)
+            freqMap[initial[key]] = curr
+        
+        for key in reversed(sorted(freqMap.keys())):
+            currList = freqMap.get(key)
+            for i in range(len(currList)):
+                result += (currList[i] * key)
+        
+        return result
